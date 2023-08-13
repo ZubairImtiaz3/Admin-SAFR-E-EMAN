21cb0611c91a657654da3488b02f77838dbef01a
Zubair Imtiaz <zubairimtiaz395@gmail.com>
2023-08-14 00:35:27 +0500

    Improve Auth guard login

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -27,6 +27,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useAuthContext } from "@/components/firebase/guard/AuthContext";
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
   const { width, breakpoints } = useWidth();
   const [collapsed] = useSidebar();
   const [isRtl] = useRtl();
@@ -58,8 +59,44 @@ export default function RootLayout({ children }) {
   const { user } = useAuthContext();
 
   useEffect(() => {
-    if (user == null) router.push("/login");
+    if (user === null) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
   }, [user]);
+
+  if (loading)
+    return (
+      <div className="flex flex-col items-center justify-center h-[100vh] app_height">
+        {/* <div className="mb-3">
+            <img src="/assets/images/logo/logo.svg" alt="Logo" />
+          </div> */}
+
+        <svg
+          className="animate-spin ltr:-ml-1 ltr:mr-3 rtl:-mr-1 rtl:ml-3 
+           h-12 w-12
+         "
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    );
 
   return (
     <div
